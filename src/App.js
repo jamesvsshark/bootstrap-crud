b12f532297053a70e85b847d45180deea5a0fdae
James Simoes <james@pocostud.io>
2018-07-31 11:51:59 -0400

    format, need linter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Bootstrap from './lib/bootstrap';
 import {Pet, Thing} from './Models';
+
 class App {
     constructor() {
         this.initialize();
@@ -9,13 +10,7 @@ class App {
         Bootstrap.InitializeCrud([Pet, Thing], {
             port: 3000
         }).then(() => {
-            // const pet = new Pet();
-            // sample data
-            // pet.model.create({
-            //     name: "TED",
-            //     breed: "CAT",
-            //     age: 4
-            // })
+          // fire away!
         });
     }
 }
